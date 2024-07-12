5236453d263328b72e67eea21bf90df728381a12
Oleksii Lisovyi <o.lisovyi@lemundo.de>
2024-07-12 17:16:08 +0300

    implement existing book update endpoint

---
+++ app/src/Model/BookUpdateDto.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Model;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+readonly class BookDto
+{
+    public function __construct(
+        #[Assert\NotBlank]
+        public string $name,
+
+        public ?string $shortDescription,
+
+        #[Assert\Date]
+        public ?\DateTimeInterface $publishedAt,
+
+        /**
+         * @var AuthorDto[]
+         */
+        #[Assert\NotBlank]
+        public array $authors
+    ) {
+    }
+}
